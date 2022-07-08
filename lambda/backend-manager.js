c655ff1f9cec934727571cd0da08b1308fabf8a8
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-07-08 22:46:46 +0000

    updated lambda/backend-manager.js.

--- lambda/backend-manager.js
+++ lambda/backend-manager.js
@@ -1,8 +1,11 @@
 const request = require("request");
 
-request({
+const getLocation = function(id) {
+    request({
     url: "https://raw.githubusercontent.com/LEGO-Baumeister/MaterialMatheaConfigs/main/test.json",
     json: true
 }, (err, response, body) => {
     console.log(body);
 });
+}
+
