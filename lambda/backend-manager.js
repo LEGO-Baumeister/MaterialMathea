e8e56509e1d3f15e25b8a8f265b1c1a75814f5e4
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-07-08 22:35:40 +0000

    updated lambda/backend-manager.js.

--- lambda/backend-manager.js
+++ lambda/backend-manager.js
@@ -1,8 +1,6 @@
-const fs = require("fs");
+const request = require("request");
 
 var data = fs.readFileSync("https://raw.githubusercontent.com/LEGO-Baumeister/MaterialMatheaConfigs/main/Test.csv", "utf8");
 
 data = data.split("\r\n");
-for (let i in data) {
-    data[i] = data[i].split(",");
-}
+fo
