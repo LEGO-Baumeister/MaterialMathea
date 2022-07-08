4a28c332c14a37f8ffc9abd77908212b74dab697
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-07-08 23:34:08 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -41,7 +41,7 @@ const GetMaterialLocationIntentHandler = {
             console.log('body:', body); // Print the body
             
             for (var i = 0; i < body.length; i++){
-                if (body[i].ID == parseInt(materialID)){
+                if (body[i].ID == 1) {
                  loc = body[i].Location;
                 }
             }
