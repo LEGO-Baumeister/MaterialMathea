aff0f2514b821f3c155a6769464cf3f36b1d2c07
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-07-09 10:45:15 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -48,6 +48,7 @@ const GetMaterialLocationIntentHandler = {
                 }
             }
             
+            loc = result.Location;
             console.log('loc_ort:', loc);
             speakOutput = `Das Material mit dem Namen ${materialName} befindet sich in Kiste Nummer ${loc}`;
         });
