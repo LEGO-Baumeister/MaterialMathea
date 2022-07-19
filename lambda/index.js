828688765959bfe943ddb16a1b9652222bfcb9d2
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-07-19 18:01:57 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -113,7 +113,9 @@ const FallbackIntentHandler = {
   },
   handle(handlerInput) {
     const speakOutput =
-      "Herzlichenglückwunsch! Du hast es Geschaft etwas zu sagen, was dieser arme FallbackIntentHandler händeln muss. Da hat Lukas wohl etwas nicht ganz durchdacht. Beschwer dich einfach bei ihm oder such selber im Repository of GitHub nach dem Fehler und erstelle ein neues Issue. Wenn du ein Gott in NodeJS bist kannst du natürlich auch direkt eine Pull Request erstellen.";
+      `Herzlichenglückwunsch! Du hast es Geschaft etwas zu sagen, was dieser arme FallbackIntentHandler händeln muss.
+        Da hat Lukas wohl etwas nicht ganz durchdacht. Beschwer dich einfach bei ihm oder such selber im Repository of GitHapp nach dem Fehler und erstelle ein neues Issue.
+        Wenn du ein Gott in NodeJottS bist kannst du natürlich auch direkt eine Pull Request erstellen.`;
 
     return handlerInput.responseBuilder
       .speak(speakOutput)
