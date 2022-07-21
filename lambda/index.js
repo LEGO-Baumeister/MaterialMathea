319da5d432be564d319a6362383be28311160071
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-07-21 09:44:33 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -223,7 +223,8 @@ const ErrorHandler = {
     return true;
   },
   handle(handlerInput, error) {
-    const speakOutput = `Oh nein! Du hast einen Error ausgelöst! Nicht nur eine Einfache Exception, sondern einen Error. Aktiviere Selbstzerstörung in 3, 2 - nein Spaß. Versuche es einfach noch einmal. Sollte der Fehler weiterhin auftreten, dann kontaktiere meinen Erbauer!`;
+    //const speakOutput = `Oh nein! Du hast einen Error ausgelöst! Nicht nur eine Einfache Exception, sondern einen Error. Aktiviere Selbstzerstörung in 3, 2 - nein Spaß. Versuche es einfach noch einmal. Sollte der Fehler weiterhin auftreten, dann kontaktiere meinen Erbauer!`;
+    const speakOutput = `Willst du noch einen Porsche, du Hurensohn?`;
     console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
 
     return handlerInput.responseBuilder
